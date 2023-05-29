c881fae3950d86d3accf105e01a60ca271831f7b
s1280145 <s1280145@u-aizu.ac.jp>
2023-05-29 20:35:38 +0900

    add crud server

---
+++ CRUDPracticeWithQuarkusAndVue/CRUDServer/src/main/java/com/example/controller/ItemController.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import com.example.entity.Item;
+import com.example.repository.ItemRepository;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/items")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Transactional
+public class ItemController {
+    @Inject
+    ItemRepository itemRepository;
+
+    @GET
+    @Path("{id}")
+    public Item getItem(@PathParam("id") Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @GET
+    public List<Item> getItemList() {
+        return itemRepository.listAll();
+    }
+
+    @POST
+    public void createItem(Item item) {
+        itemRepository.persist(item);
+    }
+
+    @PUT
+    @Path("{id}")
+    public void updateItem(@PathParam("id") Long id, Item item) {
+        Item updateItem = itemRepository.findById(id);
+        updateItem.setName(item.getName());
+        updateItem.setPrice(item.getPrice());
+        itemRepository.persist(updateItem);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteItem(@PathParam("id") Long id) {
+        itemRepository.deleteById(id);
+    }
+}
