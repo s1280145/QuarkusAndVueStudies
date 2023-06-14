bb336aba66b43e0f36786d23b502fd1087a2e4a7
s1280145 <s1280145@u-aizu.ac.jp>
2023-06-14 14:08:40 +0900

    add validation function

--- ReservationSystem/reservation_system_client/src/views/CreateReservation.vue
+++ ReservationSystem/reservation_system_client/src/views/CreateReservation.vue
@@ -22,6 +22,7 @@
             <div class="form-group">
               <label>Name:</label>
               <input type="text" class="form-control" v-model="reservation.userName" />
+              <p v-if="isInValidName(reservation.userName)" class="text-danger">この項目は必須入力です</p>
             </div>
             <div class="form-group">
               <label>Purpose:</label>
@@ -29,7 +30,7 @@
             </div>
             <input type="hidden" class="form-control" name="reservation.usageSituation" value="0" />
             <div class="form-group">
-              <input type="submit" class="btn btn-primary mt-5" value="Add Reservation" />
+              <input v-bind:disabled="noReservation" type="submit"  class="btn btn-primary mt-5" value="Add Reservation" />
             </div>
           </form>
         </div>
@@ -43,8 +44,7 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      reservation: {},
-      massage: ""
+      reservation: {}
     }
   },
   methods: {
@@ -52,6 +52,17 @@ export default {
       let uri = "/reservations"
       axios.post(uri, this.reservation)
           .then(() => {this.$router.push('/home')})
+    },
+    isInValidName(username){
+      if(username === undefined) return true;
+      return username.length < 1;
+    }
+  },
+  computed: {
+    noReservation() {
+      if(this.reservation.userName === undefined || this.reservation.purpose === undefined) return true;
+      if(this.reservation.date && this.reservation.startTime && this.reservation.endTime && this.reservation.userName.length > 0 && this.reservation.purpose.length > 0) return false;
+      return true;
     }
   }
 }
